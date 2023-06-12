76509c4b738b134555e2e73db8b8140401548daf
Minjae Kim <minjae.kim@mytrum.com>
2023-06-12 21:01:28 +0900

    Initial Commit

---
+++ kakaoauth/kakaoauthApp.swift
@@ -0,0 +1,17 @@
+//
+//  kakaoauthApp.swift
+//  kakaoauth
+//
+//  Created by Ming on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct kakaoauthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
